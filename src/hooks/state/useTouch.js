6f6ca331fb5eec2ceebd6a2cd296abd2eae33d77
chaehun <chaehun@uniess.co.kr>
2023-02-02 12:58:33 +0900

    feat : useTouch Hook - Dom event listener 설정 로직 수적

--- src/hooks/state/useTouch.js
+++ src/hooks/state/useTouch.js
@@ -6,23 +6,32 @@ const useTouch = ({ ref }) => {
   const [ dragHeight, setDragHeight ] = useState(0)
   const [ action, setAction ] = useState(DragAction.IDLE)
 
+  const handleTouchEnd = e => {
+    const { clientY } = e.changedTouches[0]
+    const height = dragStartPoint - clientY
+    if (height > 0) {  //  Drag Up
+      setAction(DragAction.UP)
+    } else {
+      setAction(DragAction.DOWN)
+    }
+    setDragHeight(height)
+  }
+
+  const handleTouchStart = e => {
+    const { clientY } = e.changedTouches[0]
+    setAction(DragAction.HOLDING)
+    setDragStartPoint(clientY)
+  }
+
   useEffect(() => {
+    const { addEventListener, removeEventListener } = ref.current
     if (ref) {
-      ref.current.ontouchstart = e => {
-        const { clientY } = e.changedTouches[0]
-        setAction(DragAction.HOLDING)
-        setDragStartPoint(clientY)
-      }
-      ref.current.ontouchend = e => {
-        const { clientY } = e.changedTouches[0]
-        const height = dragStartPoint - clientY
-        if (height > 0) {  //  Drag Up
-          setAction(DragAction.UP)
-        } else {
-          setAction(DragAction.DOWN)
-        }
-        setDragHeight(height)
-      }
+      addEventListener('touchstart', handleTouchStart)
+      addEventListener('touchend', handleTouchEnd)
+    }
+    return () => {
+      removeEventListener('touchstart', handleTouchStart)
+      removeEventListener('touchend', handleTouchEnd)
     }
   })
 
